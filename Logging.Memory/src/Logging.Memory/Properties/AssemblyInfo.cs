cf038f250018b8724905ceaae6027abe3ec54e6e
Igor Vinogradov <i_vinogradov@hotmail.com>
2018-11-18 23:26:46 +0300

    package description

--- Logging.Memory/src/Logging.Memory/Properties/AssemblyInfo.cs
+++ Logging.Memory/src/Logging.Memory/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("iflight.Logging")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Logging.Memory allows write log to memory")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Logging.Memory")]
